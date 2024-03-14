5d9efcd42a8f32284bd8e3307f1cd0f1956e39a9
unknown <abdomagdi46@gmail.com>
2024-03-14 13:00:52 +0200

    all files v2 done

--- bd-a1/vis.py
+++ bd-a1/vis.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Create a simple visualization
-def create_visualization(data):
-    plt.scatter(data['Feature1'], data['Feature2'])
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
+def create_visualization(file_path):
+    plt.scatter(data['Cabin'], data['Survived'])
+    plt.xlabel('Cabin')
+    plt.ylabel('Survived')
     plt.title('Simple Visualization')
     plt.savefig('vis.png')
     plt.close()
 
 if __name__ == "__main__":
     # Load your data
-    data = pd.read_csv('your_data.csv')
+    data = pd.read_csv('file_path')
 
     # Create visualization
     create_visualization(data)
